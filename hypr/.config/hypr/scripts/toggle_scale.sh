3a1e834db4379741a4fbe2875d95bd377619ee3d
meng <hoangm960@gmail.com>
2025-09-05 12:41:07 +0700

    hypr: add toggle scaling

---
+++ hypr/.config/hypr/scripts/toggle_scale.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MONITOR_NAME="eDP-1"
+
+current_scale=$(hyprctl monitors | grep "Monitor $MONITOR_NAME" -A 10 | grep "scale:" | awk '{print $2}')
+
+if [ "$current_scale" == "1.00" ]; then
+    new_scale="1.25"
+else
+    new_scale="1"
+fi
+
+hyprctl keyword monitor "$MONITOR_NAME,preferred,auto,$new_scale"
+
+echo "Monitor '$MONITOR_NAME' scale set to $new_scale."
