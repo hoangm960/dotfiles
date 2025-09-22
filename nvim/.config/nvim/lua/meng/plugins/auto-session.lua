983d29a497c8d5c859966e1c90bb6f7e39ed08d2
meng <hoangm960@gmail.com>
2025-09-22 11:19:01 +0700

    nvim: update auto-session

--- nvim/.config/nvim/lua/meng/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/meng/plugins/auto-session.lua
@@ -4,8 +4,9 @@ return {
 		local auto_session = require("auto-session")
 
 		auto_session.setup({
-			auto_restore_enabled = false,
-			auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+			auto_restore = false,
+			suppress_dirs = { "~/", "~/Downloads" },
+			show_auto_restore_notif = false,
 		})
 
 		vim.keymap.set("n", "<leader>wr", "<cmd>AutoSession restore<CR>", { desc = "[W]ork [R]estore" })
