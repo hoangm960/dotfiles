18d58c870746b62651821e6b9fdf2184b951a04a
hoangm960 <hoangm960@gmail.com>
2025-11-06 03:04:31 +0700

    nvim: change flash key bindings

--- nvim/.config/nvim/lua/meng/plugins/flash.lua
+++ nvim/.config/nvim/lua/meng/plugins/flash.lua
@@ -1,31 +1,31 @@
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	opts = {},
-	keys = {
-		{
-			"l",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"L",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-	},
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        {
+            "\\",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").jump()
+            end,
+            desc = "Flash",
+        },
+        {
+            "|",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").treesitter()
+            end,
+            desc = "Flash Treesitter",
+        },
+        {
+            "r",
+            mode = "o",
+            function()
+                require("flash").remote()
+            end,
+            desc = "Remote Flash",
+        },
+    },
 }
