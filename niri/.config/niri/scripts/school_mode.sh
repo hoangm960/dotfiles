912d8334d2705a3df6826d1d877dfd8451101977
hoangm960 <hoangm960@gmail.com>
2026-02-01 12:01:09 +0700

    niri: update school_mode

--- niri/.config/niri/scripts/school_mode.sh
+++ niri/.config/niri/scripts/school_mode.sh
@@ -6,8 +6,10 @@ if [ "$current_profile" == "power-saver" ]; then
     powerprofilesctl set performance
     dms ipc call audio setvolume 100
     brightnessctl set 100%
+    brightnessctl -d asus::kbd_backlight set 3
 else
     powerprofilesctl set power-saver
     dms ipc call audio setvolume 0
     brightnessctl set 50%
+    brightnessctl -d asus::kbd_backlight set 0
 fi
