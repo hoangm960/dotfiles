5ddf0627aee887b1009920e8886cf0087b16435d
hoangm960 <hoangm960@gmail.com>
2026-04-12 16:41:07 +0700

    niri: school mode change refresh rate

--- niri/.config/niri/scripts/school_mode.sh
+++ niri/.config/niri/scripts/school_mode.sh
@@ -7,9 +7,11 @@ if [ "$current_profile" == "power-saver" ]; then
     dms ipc call audio setvolume 100
     brightnessctl set 100%
     brightnessctl -d asus::kbd_backlight set 3
+    niri msg output eDP-1 mode 1920x1080@144.003
 else
     powerprofilesctl set power-saver
     dms ipc call audio setvolume 0
     brightnessctl set 50%
     brightnessctl -d asus::kbd_backlight set 0
+    niri msg output eDP-1 mode 1920x1080@60.004
 fi
