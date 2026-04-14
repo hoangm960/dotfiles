c9f9c4b9493398f8b6a60feda3917c9dba13088d
hoangm960 <hoangm960@gmail.com>
2026-04-14 14:03:57 +0700

    nvim: fix image persist when change file

--- nvim/.config/nvim/lua/meng/plugins/image.lua
+++ nvim/.config/nvim/lua/meng/plugins/image.lua
@@ -1,22 +1,24 @@
 return {
-	"3rd/image.nvim",
-	opts = {
-		backend = "kitty",
-		kitty_method = "normal",
-		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
-		integrations = {
-			markdown = {
-				enabled = true,
-				inlay_hints = { enabled = true },
-			},
-		},
-		image_provider = function(path)
-			if path:match("%.svg$") then
-				local png_path = "/tmp/rsvg_" .. vim.fn.fnamemodify(path, ":t") .. ".png"
-				vim.fn.system("rsvg-convert -o " .. png_path .. " " .. path)
-				return png_path
-			end
-			return nil
-		end,
-	},
+    "3rd/image.nvim",
+    opts = {
+        backend = "kitty",
+        kitty_method = "normal",
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+        tmux_show_only_in_active_window = true,
+        window_overlap_clear_enabled = true,
+        integrations = {
+            markdown = {
+                enabled = true,
+                inlay_hints = { enabled = true },
+            },
+        },
+        image_provider = function(path)
+            if path:match("%.svg$") then
+                local png_path = "/tmp/rsvg_" .. vim.fn.fnamemodify(path, ":t") .. ".png"
+                vim.fn.system("rsvg-convert -o " .. png_path .. " " .. path)
+                return png_path
+            end
+            return nil
+        end,
+    },
 }
